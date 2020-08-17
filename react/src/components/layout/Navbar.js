305d2a6b5a5da4042c8e0cd571918e6f10bea7b8
dmitriy shcherbachenya <dmitriyvasil@gmail.com>
2020-08-17 07:06:21 +0300

    navbar change

--- react/src/components/layout/Navbar.js
+++ react/src/components/layout/Navbar.js
@@ -12,7 +12,7 @@ class Navbar extends Component {
     render() {
         return (
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div>
                     <form class="form-inline my-2 my-lg-0">
                         <input value={this.state.value} onChange={e => this.setState({ value: e.target.value })} class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
                         <button
